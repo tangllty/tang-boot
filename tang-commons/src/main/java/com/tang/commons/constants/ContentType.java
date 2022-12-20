42a6e364c4b0221d7302513a4c87140eb438d7ee
Tang <tangllty@163.com>
2022-12-20 18:28:12 +0800

    添加相应内容类型

---
+++ tang-commons/src/main/java/com/tang/commons/constants/ContentType.java
@@ -0,0 +1,15 @@
+package com.tang.commons.constants;
+
+/**
+ * 响应内容类型
+ *
+ * @author Tang
+ */
+public class ContentType {
+
+    /**
+     * JSON字符串
+     */
+    public static final String APPLICATION_JSON = "application/json";
+
+}
