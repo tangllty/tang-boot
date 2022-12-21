cae3672f70e2deeb252c4e5b6b8399b2626ae0d8
Tang <tangllty@163.com>
2022-12-21 14:28:58 +0800

    将addAllowedOrigin替换为addAllowedOriginPattern

--- tang-framework/src/main/java/com/tang/framework/config/CorsConfig.java
+++ tang-framework/src/main/java/com/tang/framework/config/CorsConfig.java
@@ -17,20 +17,20 @@ public class CorsConfig {
     @Bean
     public CorsFilter corsFilter() {
         // 1.添加 CORS配置信息
-        var config = new CorsConfiguration();
+        var corsConfiguration = new CorsConfiguration();
         // 放行哪些原始域
-        config.addAllowedOrigin("*");
-        // 是否发送 Cookie
-        config.setAllowCredentials(true);
+        corsConfiguration.addAllowedOriginPattern(CorsConfiguration.ALL);
         // 放行哪些请求方式
-        config.addAllowedMethod("*");
-        // 放行哪些原始请求头部信息
-        config.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod(CorsConfiguration.ALL);
+        // 放行哪些请求头
+        corsConfiguration.addAllowedHeader(CorsConfiguration.ALL);
         // 暴露哪些头部信息
-        config.addExposedHeader("*");
+        corsConfiguration.addExposedHeader(CorsConfiguration.ALL);
+        // 是否发送 Cookie
+        corsConfiguration.setAllowCredentials(true);
         // 2. 添加映射路径
         var corsConfigurationSource = new UrlBasedCorsConfigurationSource();
-        corsConfigurationSource.registerCorsConfiguration("/**",config);
+        corsConfigurationSource.registerCorsConfiguration("/**",corsConfiguration);
         // 3. 返回新的CorsFilter
         return new CorsFilter(corsConfigurationSource);
     }
