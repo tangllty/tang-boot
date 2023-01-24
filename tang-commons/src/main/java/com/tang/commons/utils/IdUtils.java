522449faa2dff72ed1387d550a08f51109458282
Tang <tangllty@163.com>
2023-01-25 02:16:18 +0800

    Id 工具类

---
+++ tang-commons/src/main/java/com/tang/commons/utils/IdUtils.java
@@ -0,0 +1,20 @@
+package com.tang.commons.utils;
+
+import java.util.UUID;
+
+/**
+ * id 工具类
+ *
+ * @author Tang
+ */
+public class IdUtils {
+
+    /**
+     * 获取 uuid
+     * @return uuid
+     */
+    public static String getUUID() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+}
