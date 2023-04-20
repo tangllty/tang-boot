087f91486f2c4f46c72b14e59ad52d2ab056405c
Tang <tangllty@163.com>
2023-04-20 17:10:17 +0800

    Add stop.sh

---
+++ bin/stop.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VERSION=1.5.2
+APPLICATION=tang-admin-${VERSION}.jar
+PID=$(ps -ef | grep $APPLICATION | grep -v grep | awk '{print $2}')
+
+if [ -n "$PID" ]; then
+        kill -9 $PID
+	echo Application stop success
+else
+        echo Application not found
+fi
