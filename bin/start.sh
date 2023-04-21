96a864c3525c79138541a55b560306d11a3141df
Tang <tangllty@163.com>
2023-04-21 10:30:25 +0800

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Into the root directory of the project
+cd "$(dirname "$0")/.."
+
 VERSION=1.5.2
 APPLICATION=tang-admin-${VERSION}.jar
 PID=$(ps -ef | grep $APPLICATION | grep -v grep | awk '{print $2}')
@@ -7,7 +10,7 @@ PID=$(ps -ef | grep $APPLICATION | grep -v grep | awk '{print $2}')
 if [ -n "$PID" ]; then
 	echo Application is already running on port 8080 with PID $PID
 else
-	cd ../tang-admin/target
+	cd tang-admin/target
 	nohup java -Dlogging.config=classpath:logback.xml -jar $APPLICATION > /dev/null 2>&1 &
 	echo Application is running on port 8080 with PID $!
 fi
