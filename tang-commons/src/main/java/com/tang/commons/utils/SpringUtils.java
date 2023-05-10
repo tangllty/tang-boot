f83cd5e58974248190798702decd0b3b287cac00
Tang <tangllty@163.com>
2023-05-10 16:52:10 +0800

    代码风格统一

--- tang-commons/src/main/java/com/tang/commons/utils/SpringUtils.java
+++ tang-commons/src/main/java/com/tang/commons/utils/SpringUtils.java
@@ -73,97 +73,97 @@ public final class SpringUtils implements BeanFactoryPostProcessor, ApplicationC
     }
 
     /**
-	 * Return an instance, which may be shared or independent, of the specified bean.
-	 * <p>This method allows a Spring BeanFactory to be used as a replacement for the
-	 * Singleton or Prototype design pattern. Callers may retain references to
-	 * returned objects in the case of Singleton beans.
-	 * <p>Translates aliases back to the corresponding canonical bean name.
-	 * <p>Will ask the parent factory if the bean cannot be found in this factory instance.
+     * Return an instance, which may be shared or independent, of the specified bean.
+     * <p>This method allows a Spring BeanFactory to be used as a replacement for the
+     * Singleton or Prototype design pattern. Callers may retain references to
+     * returned objects in the case of Singleton beans.
+     * <p>Translates aliases back to the corresponding canonical bean name.
+     * <p>Will ask the parent factory if the bean cannot be found in this factory instance.
      *
-	 * @param name the name of the bean to retrieve
-	 * @return an instance of the bean
-	 * @throws NoSuchBeanDefinitionException if there is no bean with the specified name
-	 * @throws BeansException if the bean could not be obtained
-	 */
-	public static Object getBean(String name) {
+     * @param name the name of the bean to retrieve
+     * @return an instance of the bean
+     * @throws NoSuchBeanDefinitionException if there is no bean with the specified name
+     * @throws BeansException if the bean could not be obtained
+     */
+    public static Object getBean(String name) {
         return beanFactory.getBean(name);
     }
 
-	/**
-	 * Return an instance, which may be shared or independent, of the specified bean.
-	 * <p>Behaves the same as {@link #getBean(String)}, but provides a measure of type
-	 * safety by throwing a BeanNotOfRequiredTypeException if the bean is not of the
-	 * required type. This means that ClassCastException can't be thrown on casting
-	 * the result correctly, as can happen with {@link #getBean(String)}.
-	 * <p>Translates aliases back to the corresponding canonical bean name.
-	 * <p>Will ask the parent factory if the bean cannot be found in this factory instance.
+    /**
+     * Return an instance, which may be shared or independent, of the specified bean.
+     * <p>Behaves the same as {@link #getBean(String)}, but provides a measure of type
+     * safety by throwing a BeanNotOfRequiredTypeException if the bean is not of the
+     * required type. This means that ClassCastException can't be thrown on casting
+     * the result correctly, as can happen with {@link #getBean(String)}.
+     * <p>Translates aliases back to the corresponding canonical bean name.
+     * <p>Will ask the parent factory if the bean cannot be found in this factory instance.
      *
-	 * @param name the name of the bean to retrieve
-	 * @param requiredType type the bean must match; can be an interface or superclass
-	 * @return an instance of the bean
-	 * @throws NoSuchBeanDefinitionException if there is no such bean definition
-	 * @throws BeanNotOfRequiredTypeException if the bean is not of the required type
-	 * @throws BeansException if the bean could not be created
-	 */
-	public static <T> T getBean(String name, Class<T> requiredType) {
+     * @param name the name of the bean to retrieve
+     * @param requiredType type the bean must match; can be an interface or superclass
+     * @return an instance of the bean
+     * @throws NoSuchBeanDefinitionException if there is no such bean definition
+     * @throws BeanNotOfRequiredTypeException if the bean is not of the required type
+     * @throws BeansException if the bean could not be created
+     */
+    public static <T> T getBean(String name, Class<T> requiredType) {
         return beanFactory.getBean(name, requiredType);
     }
 
-	/**
-	 * Return an instance, which may be shared or independent, of the specified bean.
-	 * <p>Allows for specifying explicit constructor arguments / factory method arguments,
-	 * overriding the specified default arguments (if any) in the bean definition.
+    /**
+     * Return an instance, which may be shared or independent, of the specified bean.
+     * <p>Allows for specifying explicit constructor arguments / factory method arguments,
+     * overriding the specified default arguments (if any) in the bean definition.
      *
-	 * @param name the name of the bean to retrieve
-	 * @param args arguments to use when creating a bean instance using explicit arguments
-	 * (only applied when creating a new instance as opposed to retrieving an existing one)
-	 * @return an instance of the bean
-	 * @throws NoSuchBeanDefinitionException if there is no such bean definition
-	 * @throws BeanDefinitionStoreException if arguments have been given but
-	 * the affected bean isn't a prototype
-	 * @throws BeansException if the bean could not be created
-	 */
-	public static Object getBean(String name, Object... args) {
+     * @param name the name of the bean to retrieve
+     * @param args arguments to use when creating a bean instance using explicit arguments
+     * (only applied when creating a new instance as opposed to retrieving an existing one)
+     * @return an instance of the bean
+     * @throws NoSuchBeanDefinitionException if there is no such bean definition
+     * @throws BeanDefinitionStoreException if arguments have been given but
+     * the affected bean isn't a prototype
+     * @throws BeansException if the bean could not be created
+     */
+    public static Object getBean(String name, Object... args) {
         return beanFactory.getBean(name, args);
     }
 
-	/**
-	 * Return the bean instance that uniquely matches the given object type, if any.
-	 * <p>This method goes into {@link ListableBeanFactory} by-type lookup territory
-	 * but may also be translated into a conventional by-name lookup based on the name
-	 * of the given type. For more extensive retrieval operations across sets of beans,
-	 * use {@link ListableBeanFactory} and/or {@link BeanFactoryUtils}.
+    /**
+     * Return the bean instance that uniquely matches the given object type, if any.
+     * <p>This method goes into {@link ListableBeanFactory} by-type lookup territory
+     * but may also be translated into a conventional by-name lookup based on the name
+     * of the given type. For more extensive retrieval operations across sets of beans,
+     * use {@link ListableBeanFactory} and/or {@link BeanFactoryUtils}.
      *
-	 * @param requiredType type the bean must match; can be an interface or superclass
-	 * @return an instance of the single bean matching the required type
-	 * @throws NoSuchBeanDefinitionException if no bean of the given type was found
-	 * @throws NoUniqueBeanDefinitionException if more than one bean of the given type was found
-	 * @throws BeansException if the bean could not be created
-	 * @see ListableBeanFactory
-	 */
-	public static <T> T getBean(Class<T> requiredType) {
+     * @param requiredType type the bean must match; can be an interface or superclass
+     * @return an instance of the single bean matching the required type
+     * @throws NoSuchBeanDefinitionException if no bean of the given type was found
+     * @throws NoUniqueBeanDefinitionException if more than one bean of the given type was found
+     * @throws BeansException if the bean could not be created
+     * @see ListableBeanFactory
+     */
+    public static <T> T getBean(Class<T> requiredType) {
         return beanFactory.getBean(requiredType);
     }
 
-	/**
-	 * Return an instance, which may be shared or independent, of the specified bean.
-	 * <p>Allows for specifying explicit constructor arguments / factory method arguments,
-	 * overriding the specified default arguments (if any) in the bean definition.
-	 * <p>This method goes into {@link ListableBeanFactory} by-type lookup territory
-	 * but may also be translated into a conventional by-name lookup based on the name
-	 * of the given type. For more extensive retrieval operations across sets of beans,
-	 * use {@link ListableBeanFactory} and/or {@link BeanFactoryUtils}.
+    /**
+     * Return an instance, which may be shared or independent, of the specified bean.
+     * <p>Allows for specifying explicit constructor arguments / factory method arguments,
+     * overriding the specified default arguments (if any) in the bean definition.
+     * <p>This method goes into {@link ListableBeanFactory} by-type lookup territory
+     * but may also be translated into a conventional by-name lookup based on the name
+     * of the given type. For more extensive retrieval operations across sets of beans,
+     * use {@link ListableBeanFactory} and/or {@link BeanFactoryUtils}.
      *
-	 * @param requiredType type the bean must match; can be an interface or superclass
-	 * @param args arguments to use when creating a bean instance using explicit arguments
-	 * (only applied when creating a new instance as opposed to retrieving an existing one)
-	 * @return an instance of the bean
-	 * @throws NoSuchBeanDefinitionException if there is no such bean definition
-	 * @throws BeanDefinitionStoreException if arguments have been given but
-	 * the affected bean isn't a prototype
-	 * @throws BeansException if the bean could not be created
-	 */
-	public static <T> T getBean(Class<T> requiredType, Object... args) {
+     * @param requiredType type the bean must match; can be an interface or superclass
+     * @param args arguments to use when creating a bean instance using explicit arguments
+     * (only applied when creating a new instance as opposed to retrieving an existing one)
+     * @return an instance of the bean
+     * @throws NoSuchBeanDefinitionException if there is no such bean definition
+     * @throws BeanDefinitionStoreException if arguments have been given but
+     * the affected bean isn't a prototype
+     * @throws BeansException if the bean could not be created
+     */
+    public static <T> T getBean(Class<T> requiredType, Object... args) {
         return beanFactory.getBean(requiredType, args);
     }
 
