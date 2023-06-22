b8e081e88d35cb9a505dd2ae762e7dca2b489eb4
Tang <tangllty@163.com>
2023-06-22 12:11:48 +0800

    生成索引文件优化启动速度

--- tang-framework/src/main/java/com/tang/framework/config/ApplicationConfig.java
+++ tang-framework/src/main/java/com/tang/framework/config/ApplicationConfig.java
@@ -2,12 +2,14 @@ package com.tang.framework.config;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Indexed;
 
 /**
  * 程序注解配置
  *
  * @author Tang
  */
+@Indexed
 @Configuration
 @MapperScan("com.tang.**.mapper")
 public class ApplicationConfig {
