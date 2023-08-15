761d956cfb5ce99ed21c7032afcdcfb13c4cd805
Tang <tangllty@163.com>
2023-08-15 16:33:05 +0800

    修复自动填充可能不存在对应方法问题

--- tang-framework/src/main/java/com/tang/framework/interceptor/AutoFillInterceptor.java
+++ tang-framework/src/main/java/com/tang/framework/interceptor/AutoFillInterceptor.java
@@ -1,6 +1,9 @@
 package com.tang.framework.interceptor;
 
+import java.lang.reflect.Method;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.apache.ibatis.executor.Executor;
 import org.apache.ibatis.mapping.MappedStatement;
@@ -21,6 +24,14 @@ import com.tang.commons.utils.SecurityUtils;
 })
 public class AutoFillInterceptor implements Interceptor {
 
+    private static final String SET_CREATE_BY = "setCreateBy";
+
+    private static final String SET_CREATE_TIME = "setCreateTime";
+
+    private static final String SET_UPDATE_BY = "setUpdateBy";
+
+    private static final String SET_UPDATE_TIME = "setUpdateTime";
+
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
         var invocationArgs = invocation.getArgs();
@@ -31,19 +42,34 @@ public class AutoFillInterceptor implements Interceptor {
         switch (sqlCommandType) {
             case INSERT -> {
                 var clazz = invocationObject.getClass();
-                clazz.getMethod("setCreateBy", String.class).invoke(invocationObject, SecurityUtils.getUser().getUsername());
-                clazz.getMethod("setCreateTime", LocalDateTime.class).invoke(invocationObject, LocalDateTime.now());
+                if (hasMethod(clazz, SET_CREATE_BY)) {
+                    clazz.getMethod(SET_CREATE_BY, String.class).invoke(invocationObject, SecurityUtils.getUser().getUsername());
+                }
+                if (hasMethod(clazz, SET_CREATE_TIME)) {
+                    clazz.getMethod(SET_CREATE_TIME, LocalDateTime.class).invoke(invocationObject, LocalDateTime.now());
+                }
             }
             case UPDATE -> {
                 var clazz = invocationObject.getClass();
-                clazz.getMethod("setUpdateBy", String.class).invoke(invocationObject, SecurityUtils.getUser().getUsername());
-                clazz.getMethod("setUpdateTime", LocalDateTime.class).invoke(invocationObject, LocalDateTime.now());
+                if (hasMethod(clazz, SET_UPDATE_BY)) {
+                    clazz.getMethod(SET_UPDATE_BY, String.class).invoke(invocationObject, SecurityUtils.getUser().getUsername());
+                }
+                if (hasMethod(clazz, SET_UPDATE_TIME)) {
+                    clazz.getMethod(SET_UPDATE_TIME, LocalDateTime.class).invoke(invocationObject, LocalDateTime.now());
+                }
             }
             default -> {
             }
         }
 
         return invocation.proceed();
+    }
+
+    private boolean hasMethod(Class<?> clazz, String method) {
+        var methods = new ArrayList<Method>();
+        methods.addAll(Arrays.asList(clazz.getDeclaredMethods()));
+        methods.addAll(Arrays.asList(clazz.getSuperclass().getDeclaredMethods()));
+        return methods.stream().anyMatch(m -> m.getName().equals(method));
     }
 
 }
