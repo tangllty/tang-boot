8ac9667f680ac90520239c7bc5c9bc02df23dd84
Tang <tangllty@163.com>
2024-01-10 16:04:47 +0800

    获取微信群二维码

--- tang-admin/src/main/java/com/tang/admin/controller/IndexController.java
+++ tang-admin/src/main/java/com/tang/admin/controller/IndexController.java
@@ -1,11 +1,20 @@
 package com.tang.admin.controller;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tang.commons.utils.AjaxResult;
 import com.tang.system.service.log.SysLogLoginService;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * 首页逻辑控制层
@@ -17,6 +26,8 @@ import com.tang.system.service.log.SysLogLoginService;
 public class IndexController {
 
     private final SysLogLoginService logLoginService;
+
+    private final HttpClient httpClient = HttpClient.newBuilder().build();
 
     public IndexController(SysLogLoginService logLoginService) {
         this.logLoginService = logLoginService;
@@ -31,6 +42,48 @@ public class IndexController {
     public AjaxResult userVisit() {
         var userVisit = logLoginService.selectUserVisit();
         return AjaxResult.success(userVisit);
+    }
+
+    /**
+     * 获取微信二维码(Gitee)
+     *
+     * @param response 响应
+     * @throws IOException          IO异常
+     * @throws InterruptedException 中断异常
+     * @throws URISyntaxException   URI异常
+     */
+    @GetMapping("/get-wechat-gitee")
+    public void getWechatGitee(HttpServletResponse response) throws IOException, InterruptedException, URISyntaxException {
+        var url = "https://gitee.com/tangllty/tang-docs/raw/master/docs/public/wechat.png";
+        var requestResult = HttpRequest.newBuilder()
+            .uri(new URI(url))
+            .GET()
+            .build();
+
+        var responseResult = httpClient.send(requestResult, HttpResponse.BodyHandlers.ofByteArray());
+        response.setContentType("image/png");
+        response.getOutputStream().write(responseResult.body());
+    }
+
+    /**
+     * 获取微信二维码(Github)
+     *
+     * @param response 响应
+     * @throws IOException          IO异常
+     * @throws InterruptedException 中断异常
+     * @throws URISyntaxException   URI异常
+     */
+    @GetMapping("/get-wechat-github")
+    public void getWechatGithub(HttpServletResponse response) throws IOException, InterruptedException, URISyntaxException {
+        var url = "https://raw.githubusercontent.com/tangllty/tang-docs/master/docs/public/wechat.png";
+        var requestResult = HttpRequest.newBuilder()
+            .uri(new URI(url))
+            .GET()
+            .build();
+
+        var responseResult = httpClient.send(requestResult, HttpResponse.BodyHandlers.ofByteArray());
+        response.setContentType("image/png");
+        response.getOutputStream().write(responseResult.body());
     }
 
 }
