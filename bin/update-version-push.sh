2bdcc0b4adbcaa376c3e3110c2347f8b91b5cac1
Tang <tangllty@163.com>
2024-02-03 01:51:43 +0800

    Update update-version-push.sh

--- bin/update-version-push.sh
+++ bin/update-version-push.sh
@@ -3,17 +3,17 @@
 source $(dirname "$0")/update-version.sh
 
 # Push the changes to the remote repository
-git add pom.xml
-for module in $(ls | grep '^tang-')
+find . -name 'pom.xml' | while read -r pomfile
 do
-	git add "${module}/pom.xml"
-	echo "${module}/pom.xml"
+  git add "${pomfile}"
+  echo "${pomfile}"
 done
 
 git add tang-admin/src/main/resources/application.yml
+git add docker-compose.yml
 
 git commit -m "v${NEW_VERSION}"
-git tag "v${NEW_VERSION}"
+git tag -a "v${NEW_VERSION}" -m "v${NEW_VERSION}"
 
 git push
 git push --tags
