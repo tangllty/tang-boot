50dc6435f42e46f18645f0f5f2e639337438539a
Tang <tangllty@163.com>
2024-02-03 01:51:34 +0800

    Update update-version.sh

--- bin/update-version.sh
+++ bin/update-version.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Into the root directory of the project
-cd "$(dirname "$0")/.."
+cd "$(dirname "$0")/.." || exit
 
 NEW_VERSION=$1
 
@@ -9,21 +9,21 @@ PATTERN="^[0-9]+\.[0-9]+\.[0-9]+$"
 
 # Check the number is valid
 if [[ ! $NEW_VERSION =~ $PATTERN ]]; then
-	echo "Invalid version number: $NEW_VERSION"
+	echo "Invalid version number: $NEW_VERSION. The version number should match the pattern: major.minor.patch (e.g. 1.0.0)"
 	exit 1
 fi
 
 # Get the old version number
 OLD_VERSION=$(sed -n 's/.*<tang.version>\(.*\)<\/tang.version>.*/\1/p' pom.xml)
 
-# Modify the version of the parent module and the tang.version of the properties
-sed -i "0,/<version>[^<]*<\/version>/ s/<version>[^<]*<\/version>/<version>${NEW_VERSION}<\/version>/; s/<tang.version>[^<]*<\/tang.version>/<tang.version>${NEW_VERSION}<\/tang.version>/g" pom.xml
+# Modify tang.version of property in pom.xml
+sed -i "s/<tang.version>[^<]*<\/tang.version>/<tang.version>${NEW_VERSION}<\/tang.version>/g" pom.xml
 
-# Modify the version of the sub-module
-for module in $(ls | grep '^tang-')
+# Modify all pom.xml files in the project
+find . -name 'pom.xml' | while read -r pomfile
 do
-	sed -i "0,/<version>[^<]*<\/version>/ s/<version>[^<]*<\/version>/<version>${NEW_VERSION}<\/version>/" "${module}/pom.xml"
-	echo "Version number changed from ${OLD_VERSION} to ${NEW_VERSION} in ${module}/pom.xml"
+  sed -i "0,/<version>[^<]*<\/version>/ s/<version>[^<]*<\/version>/<version>${NEW_VERSION}<\/version>/" "${pomfile}"
+  echo "Version number changed from ${OLD_VERSION} to ${NEW_VERSION} in ${pomfile}"
 done
 
 # Define the application yml file path
